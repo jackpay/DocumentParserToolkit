8f94ab5a2bc5812d61f3a2a54e6622358ec34881
Jack <jp242@sussex.ac.uk>
2014-01-31 16:53:11 +0000

    refactor registry

--- src/ac/uk/susx/tag/annotator/factory/AbstractAnnotatorFactory.java
+++ src/ac/uk/susx/tag/annotator/factory/AbstractAnnotatorFactory.java
@@ -5,9 +5,13 @@ import ac.uk.susx.tag.annotator.registry.AnnotatorRegistry;
 
 public abstract class AbstractAnnotatorFactory {
 	
-	public AbstractAnnotatorFactory(String id) {
-		AnnotatorRegistry.register(id, create());
+	public static final String ID;
+	
+	static {
+		AnnotatorRegistry.register(ID);
 	}
+	
+	public abstract String id();
 	
 	public abstract <AT,DT,ACT> IAnnotator<AT,DT,ACT> create();
 	
