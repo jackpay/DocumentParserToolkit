2d862a5b7820b8ec2355434757faae0db7893efb
Jack <jack@Jack-Ubuntu-PC.(none)>
2014-04-06 21:28:53 +0100

    finish entity linking annotator

--- src/ac/uk/susx/tag/annotator/EntityLinkingAnnotator.java
+++ src/ac/uk/susx/tag/annotator/EntityLinkingAnnotator.java
@@ -5,10 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import ac.uk.susx.tag.annotation.IAnnotation;
 import ac.uk.susx.tag.annotation.StringAnnotation;
@@ -17,41 +17,50 @@ import ac.uk.susx.tag.utils.IncompatibleAnnotationException;
 
 public class EntityLinkingAnnotator extends AbstractStringAnnotator {
 	
-	private HashSet<List<? extends IAnnotation<String>>> entities;
+	private HashMap<IAnnotation<String>, List<List<? extends IAnnotation<String>>>> entities;
 	private static final String TAG = "entity";
 	private static final String DELIM = "_";
 	
 	public EntityLinkingAnnotator() {
-		entities = new HashSet<List<? extends IAnnotation<String>>>();
+		entities = new HashMap<IAnnotation<String>, List<List<? extends IAnnotation<String>>>>();
 	}
 
 	public List<StringAnnotation> annotate(IAnnotation<String> annotation) throws IncompatibleAnnotationException {
 		ArrayList<StringAnnotation> annotations = new ArrayList<StringAnnotation>();
 		List<? extends IAnnotation<String>> ann = StringAnnotatorEnum.TOKEN.getAnnotator().annotate(annotation);
-		for(IAnnotation<String> a : ann) {
-			if(entities.contains(a)) {
-				
-			}
-		}
-		for(List<? extends IAnnotation<String>> entity : entities) {
-			if(ann.contains(entity) && entity.length() > 3) {
-				System.out.println("FOUND: " + entity + " " + ann.indexOf(entity)  + " " + ann.substring(ann.indexOf(entity)));
-				boolean found = true;
-				int i = 0;
-				while(i < ann.length() && found) {
-					Pattern patt = Pattern.compile(entity);
-					Matcher match = patt.matcher(ann.substring(i));
-					if(match.matches()){
-						String strEntity = ann.substring(match.start(), match.end());
-						System.out.println(strEntity);
-						strEntity = strEntity.replace("\\s* ", DELIM).replace("\t", DELIM) + DELIM + TAG;
-						annotations.add(new StringAnnotation(strEntity,match.start(),match.end()));
-						i = match.end();
+		int i = 0;
+		while(i < ann.size()) {
+			List<List<? extends IAnnotation<String>>> anns = entities.get(ann.get(i));
+			if(anns != null) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(ann.get(i)+ DELIM);
+				int offsetEnd = ann.get(i).getEnd();
+				innerloop: // label the loop to ensure correct breaking.
+				for(List<? extends IAnnotation<String>> annL : anns) {
+					StringBuilder sb2 = new StringBuilder();
+					boolean found = true;
+					int j = 0;
+					whileloop:
+					while(found && j < annL.size()) {
+						found = annL.get(j).getAnnotation().equals(ann.get(i+j).getAnnotation()) ? true : false;
+						j++;
+						sb2.append(annL.get(j) + DELIM);
+						if(!found) {
+							break whileloop;
+						}
 					}
-					else{
-						found = false;
+					if(found) {
+						sb.append(sb2.toString() + DELIM);
+						offsetEnd = annL.get(j).getEnd();
+						i += j;
+						break innerloop;
 					}
 				}
+				sb.append(TAG);
+				annotations.add(new StringAnnotation(sb.toString(),ann.get(i).getStart(),offsetEnd));
+			}
+			else {
+				i++;
 			}
 		}
 		return annotations;
@@ -65,7 +74,14 @@ public class EntityLinkingAnnotator extends AbstractStringAnnotator {
 				line = br.readLine();
 				while(line != null) {
 					try {
-						entities.add(StringAnnotatorEnum.TOKEN.getAnnotator().annotate(new StringAnnotation(line.replace("\n", ""),0,0)));
+						List<? extends IAnnotation<String>> entity = StringAnnotatorEnum.TOKEN.getAnnotator().annotate(new StringAnnotation(line.replace("\n", ""),0,0));
+						if(entities.get(entity.get(0)) == null) {
+							entities.put(entity.get(0), new ArrayList<List<? extends IAnnotation<String>>>());
+						}
+						if(entity.size() > 1) {
+							entities.get(entity.get(0)).add(entity.subList(1, entity.size()));
+						}
+						Collections.sort(entities.get(entity.get(0)), new ListSorter());
 					} catch (IncompatibleAnnotationException e) {
 						e.printStackTrace();
 					}
@@ -83,5 +99,14 @@ public class EntityLinkingAnnotator extends AbstractStringAnnotator {
 		return entities == null ? false : true;
 	}
 
-	
+	private class ListSorter implements Comparator<List<? extends IAnnotation<String>>> {
+
+		public int compare(List<? extends IAnnotation<String>> o1, List<? extends IAnnotation<String>> o2) {
+			if(o1.size() == o2.size()) {
+				return 0;
+			}
+			return o1.size() > o2.size() ? -1 : 1;
+		}
+		
+	}
 }
